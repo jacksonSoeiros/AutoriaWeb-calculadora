c789a7ae1ac60ee9ea9e9228c1fbfd47aa87568c
jackson-soerios <soeiros.jackson.fonseca@gmail.com>
2023-05-26 04:17:48 -0300

    remeção de condigo desnecessario

--- script.js
+++ script.js
@@ -26,35 +26,3 @@ function del(){
     let result2  = result.value;
     result.value = result2.substring(0, result2.length - 1);
 }
-/*<script>
-      let input = document.getElementById("result");
-
-      function appendNumber(number) {
-        input.value += number;
-      }
-
-      function calculate() {
-        let expression = input.value;
-        let result = eval(expression);
-        input.value = result;
-      }
-      function add() {
-        var result = appendNumber + appendNumber;
-        document.getElementById("result").textContent = result;
-      }
-
-      function subtract() {
-        var result = appendNumber - appendNumber;
-        document.getElementById("result").textContent = result;
-      }
-
-      function multiply() {
-        var result = appendNumber * appendNumber;
-        document.getElementById("result").textContent = result;
-      }
-
-      function divide() {
-        var result = appendNumber / appendNumber;
-        document.getElementById("result").textContent = result;
-      }
-    </script> */
